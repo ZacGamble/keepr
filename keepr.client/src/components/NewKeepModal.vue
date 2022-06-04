4225fbb1dc51b5bf196f41e6ebb4b17514108b6b
ZacGamble <zacrgamble@gmail.com>
2022-06-04 17:51:09 -0600

    create keep

--- keepr.client/src/components/NewKeepModal.vue
+++ keepr.client/src/components/NewKeepModal.vue
@@ -3,7 +3,7 @@
     <template #modal-body-slot>
       <div class="d-flex-flex-column">
         <h1>New Keep</h1>
-        <form class="fs-5">
+        <form class="fs-5" @submit.prevent="createNewKeep()">
           <div class="d-flex flex-column">
             <input
               type="text"
@@ -12,17 +12,20 @@
               minlength="3"
               maxlength="20"
               class="my-3 p-2 rounded"
+              v-model="formData.name"
             />
             <input
               type="url"
               placeholder="URL..."
               class="my-3 p-2 rounded"
               required
+              v-model="formData.img"
             />
             <textarea
               placeholder="Keep description"
               maxlength="120"
               class="p-2 my-2 rounded"
+              v-model="formData.description"
             ></textarea>
           </div>
           <button class="btn btn-primary">Create</button>
@@ -34,9 +37,30 @@
 
 
 <script>
+import { ref } from '@vue/reactivity'
+import { keepsService } from '../services/KeepsService'
+import { logger } from '../utils/Logger'
+import Pop from '../utils/Pop'
+import { Modal } from 'bootstrap'
 export default {
   setup() {
-    return {}
+    const formData = ref({})
+
+    return {
+      formData,
+
+      async createNewKeep() {
+        try {
+
+          await keepsService.create(formData.value)
+          Modal.getOrCreateInstance(document.getElementById('new-keep-modal')).hide()
+          Pop.toast("Creation success!", 'success')
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
+        }
+      }
+    }
   }
 }
 </script>
--- keepr.client/src/services/KeepsService.js
+++ keepr.client/src/services/KeepsService.js
@@ -3,6 +3,11 @@ import { logger } from "../utils/Logger";
 import { api } from "./AxiosService";
 
 class KeepsService {
+    async create(formData){
+        const res = await api.post("api/keeps", formData)
+        AppState.userKeeps.unshift(res.data)
+        logger.log(res.data)
+    }
     async getAll(){
         const res = await api.get("api/keeps")
         AppState.keeps = res.data;
